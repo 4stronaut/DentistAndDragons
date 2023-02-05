e9adf63ccc23e748e13aaa437c719aa5fb3d0358
Steffen B <steffen@steff.en>
2023-02-05 04:48:57 +0100

    Added initial pulling roots functionality

---
+++ DentistAndDragons_Unity/Assets/Scripts/CustomRootGrabbable.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomRootGrabbable : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _yOffsetGameObject;
+
+    [SerializeField]
+    private Rigidbody _leftHandRigidbody;
+
+    private float _accumulatedDeltaRotation = 0f;
+    [SerializeField]
+    private float _maxWiggleToPull = 2000f;
+
+    [SerializeField]
+    private GameObject _leftHandAnchor;
+
+    [SerializeField]
+    private GameObject destructionEffect;
+
+    private Vector3 _startPos;
+    private Quaternion _currentRot;
+
+    private Vector3 _lastPos;
+    private Quaternion _lastRot;
+
+    public enum RootState
+    {
+        Released,
+        Grabbed,
+        Held
+    }
+    private RootState _state = RootState.Released;
+
+
+    private void Start()
+    {
+        _lastPos = _leftHandAnchor.transform.position;
+        _lastRot = _leftHandAnchor.transform.rotation;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (_state == RootState.Released && OVRInput.Get(OVRInput.Button.PrimaryHandTrigger))
+        {
+            _state = RootState.Grabbed;
+
+            _startPos = Vector3.Normalize(_leftHandAnchor.transform.position - this.transform.position);
+            _currentRot = this.transform.rotation;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (!OVRInput.Get(OVRInput.Button.PrimaryHandTrigger))
+        {
+            if (_state == RootState.Held)
+            {
+                this.transform.SetParent(null);
+                Rigidbody rb = this.gameObject.AddComponent<Rigidbody>();
+                if (_leftHandRigidbody)
+                {
+                    Vector3 newpos = _leftHandAnchor.transform.position;
+                    Vector3 diff = newpos - _lastPos;
+                    rb.velocity = diff / Time.deltaTime;
+                    rb.angularVelocity = Vector3.one * 2;
+                }
+                rb.useGravity = true;
+                this.GetComponent<Collider>().isTrigger = false;
+            }
+
+            _state = RootState.Released;
+        }
+
+        if (_state == RootState.Grabbed)
+        {
+            Vector3 closestPoint = Vector3.Normalize(_leftHandAnchor.transform.position - this.transform.position);
+            this.transform.rotation = Quaternion.FromToRotation(_startPos, closestPoint) * _currentRot;
+
+            float deltaAngle = Quaternion.Angle(_lastRot, _leftHandAnchor.transform.rotation);
+            _accumulatedDeltaRotation += deltaAngle;
+            _lastRot = _leftHandAnchor.transform.rotation;
+
+            if (_yOffsetGameObject)
+                _yOffsetGameObject.transform.localPosition = new Vector3(0f, 0.3f * _accumulatedDeltaRotation / _maxWiggleToPull, 0f);
+
+            if (_accumulatedDeltaRotation >= _maxWiggleToPull)
+            {
+                this.transform.SetParent(_leftHandAnchor.transform);
+                _state = RootState.Held;
+
+                Instantiate(destructionEffect, _yOffsetGameObject.transform.position, Quaternion.LookRotation(-this.transform.up));
+            }
+            
+        }
+
+        _lastPos = _leftHandAnchor.transform.position;
+    }
+
+}
