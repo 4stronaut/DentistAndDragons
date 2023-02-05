bde5e574262767013c0750c9fe1f6c7d345fa9af
Max <maxpherz@gmail.com>
2023-02-05 13:27:40 +0100

    animations working

--- DentistAndDragons_Unity/Assets/Scripts/WorldController.cs
+++ DentistAndDragons_Unity/Assets/Scripts/WorldController.cs
@@ -14,6 +14,8 @@ public class WorldController : MonoBehaviour
     {
         startTime = DateTime.UtcNow;
         started = true;
+        bobbert.hurt();
+        bobbert.openMouth();
     }
 
     private void Update()
